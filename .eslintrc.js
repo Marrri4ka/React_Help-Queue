13f5b67f4f7da310810905076cd488baa15e1627
Mariia <mariiapopovych@gmail.com>
2019-06-29 22:27:48 -0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	"env": {
+		"browser": true
+	},
+	"extends": "eslint:recommended",
+	"rules": {
+		"react/jsx-uses-vars": 2,
+
+
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+
+    "plugins": [
+     "react"
+ ],
+	}
+};
