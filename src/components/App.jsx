ac69057518369502bd3f30eeb2dbc5be5fd635cd
Mariia <mariiapopovych@gmail.com>
2019-07-15 11:15:34 -0700

    mapped redux state to react props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,11 @@ import Header from './Header';
 import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter} from 'react-router-dom';
 import Moment from 'moment';
 import Admin from  './Admin';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 
 
@@ -14,7 +16,6 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      masterTicketList: {},
       selectedTicket: null
     };
 
@@ -48,15 +49,16 @@ class App extends React.Component {
 
 
   render(){
-     console.log(this.state.masterTicketList);
+
     return (
 
       <div>
         <Header/>
           <Switch>
-          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
-          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
-          <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname}
+          <Route exact path='/' render={()=><TicketList ticketList={this.props.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl />} />
+          <Route path='/admin' render={(props)=><Admin ticketList={this.props.masterTicketList}
+             currentRouterPath={props.location.pathname}
             onTicketSelection={this.handleChangingSelectedTicket}
             selectedTicket={this.state.selectedTicket} />} />
           <Route component={Error404} />
@@ -64,7 +66,20 @@ class App extends React.Component {
       </div>
     );
   }
-
 }
 
-export default App;
+App.propTypes={
+  masterTicketList: PropTypes.object
+
+};
+
+  const mapStateToProps = state=>{
+    return {
+      masterTicketList: state
+    }
+  }
+
+
+
+
+  export default withRouter(connect(mapStateToProps)(App));
--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import errorPic from '../assets/img/Error.png';
+
 
 
 function Error404(props){
